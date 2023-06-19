d155c8fdaf53c7e762b62f82249365eb11301eb1
aj1305 Abdullah <aj1305@york.ac.uk>
2023-06-19 22:16:28 +0100

    Added viewspeed V2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,55 +7,40 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  dpiControl = new FormControl();
-  screenWidthControl = new FormControl();
-  fieldOfViewControl = new FormControl();
-  sensInInch: number | undefined;
-  sensInCm: number | undefined;
+  baseSensControl = new FormControl();
+  baseFovControl = new FormControl();
+  newFovControl = new FormControl();
+  newSens: number | undefined;
 
   ngOnInit() {
-    this.dpiControl.setValue(localStorage.getItem('dpi'));
-    this.screenWidthControl.setValue(localStorage.getItem('screenWidth'));
-    this.fieldOfViewControl.setValue(localStorage.getItem('fov'));
-    const sensInInch = localStorage.getItem('sensInInch');
-    const sensInCm = localStorage.getItem('sensInCm');
-    this.sensInInch = sensInInch ? Number(sensInInch) : undefined;
-    this.sensInCm = sensInCm ? Number(sensInCm) : undefined;
+    this.baseSensControl.setValue(localStorage.getItem('baseSens'));
+    this.baseFovControl.setValue(localStorage.getItem('baseFov'));
+    this.newFovControl.setValue(localStorage.getItem('newFov'));
+    const newSens = localStorage.getItem('newSens');
+    this.newSens = newSens ? Number(newSens) : undefined;
   }
 
   calculate() {
     if (
-      !this.dpiControl.value ||
-      !this.screenWidthControl.value ||
-      !this.fieldOfViewControl.value
+      !this.baseSensControl.value ||
+      !this.baseFovControl.value ||
+      !this.newFovControl.value
     ) {
-      this.sensInInch = undefined;
-      this.sensInCm = undefined;
+      this.newSens = undefined;
       return;
     }
 
-    const dpi = this.dpiControl.value;
-    const screenWidth = this.screenWidthControl.value;
-    const fov = this.fieldOfViewControl.value;
+    const baseSens = this.baseSensControl.value;
+    const baseFov = this.baseFovControl.value;
+    const newFov = this.newFovControl.value;
 
-    const d = screenWidth / dpi;
+    const ratio = Math.sin(this.degToRadians(baseFov * Math.PI/360)) / Math.sin(this.degToRadians(newFov * Math.PI/360))
+    this.newSens = this.roundNum(ratio * baseSens);
 
-    const firstHalf = d * Math.PI;
-    const secondHalf =
-      Math.cos((Math.PI * this.degToRadians(fov)) / this.degToRadians(720)) *
-      (1 -
-        Math.cos((Math.PI * this.degToRadians(fov)) / this.degToRadians(360)) +
-        Math.sin((Math.PI * this.degToRadians(fov)) / this.degToRadians(360)));
-
-    const sensInInches = firstHalf / secondHalf;
-    this.sensInInch = this.roundNum(sensInInches);
-    this.sensInCm = this.roundNum(sensInInches * 2.54);
-
-    localStorage.setItem('sensInInch', this.sensInInch?.toString());
-    localStorage.setItem('sensInCm', this.sensInCm?.toString());
-    localStorage.setItem('dpi', dpi);
-    localStorage.setItem('fov', fov);
-    localStorage.setItem('screenWidth', screenWidth);
+    localStorage.setItem('newSens', this.newSens?.toString());
+    localStorage.setItem('baseSens', baseSens);
+    localStorage.setItem('newFov', newFov);
+    localStorage.setItem('baseFov', baseFov);
   }
 
   copy(sens: number | undefined) {
